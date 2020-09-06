d2bc0489f34df9cc0c40e38d2dd160e7fe4e257c
Alexander799 <sashkakovalev63@mail.com>
2020-09-06 18:58:05 +0300

    added carousel code for reviews

--- js/my-scripts/my-script.js
+++ js/my-scripts/my-script.js
@@ -31,4 +31,21 @@ $(document).ready(function() {
             }
         }
     })
+
+    $('.owl-client').owlCarousel({
+        animateOut: 'fadeOut',
+        loop: true,
+        autoplayHoverPause: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    })
 });
